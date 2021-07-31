5c5d5899838bc38f404e05c9cfb9384ae626ca7d
Jacob <jacob.saini60@gmail.com>
2021-07-31 16:54:46 +1000

    add post route file upload and timeframe, add flask_compress, change some var names in atr calculations

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 from flask import Flask
+from flask import request
 import pandas
 from calculations import *
+from flask_compress import Compress
+
+compress = Compress()
+
 app = Flask(__name__)
-
-
+compress.init_app(app)
 
 @app.route("/")
 def hello():
     df = pandas.read_csv("AUDJPY=X.csv")
-    # histogram = histogram(df)
-    atr = atr(df)
-    print(df)
-    return 
+    stats = {
+        "histogram":histogram(df),
+        "ATR":atr(df,"Daily")
+    }
+
+    return stats
+
 
     
-@app.route("/calculate", methods=['GET', 'POST'])
+@app.route("/calculate", methods=['POST'])
 def calculate():
-    return "<p>Hello</p>"
+    df = pandas.read_csv(request.files['file'])
+    timeframe = request.form['timeframe']
+    stats = {
+        "histogram":histogram(df),
+        "ATR":atr(df,timeframe)
+    }
+
+    return stats
--- calculations.py
+++ calculations.py
@@ -1,16 +1,10 @@
 import pandas
-df = pandas.read_csv("AUDJPY=X.csv")
-df['Date'] = pandas.to_datetime(df.Date)
-df.sort_values(by="Date", inplace=True, ascending=False)
-
-dfwk = pandas.read_csv("AUDJPYWK.csv")
-dfwk.sort_values(by="Date", inplace=True, ascending=False)
-
-dfmon = pandas.read_csv("AUDJPYMON.csv")
-dfmon.sort_values(by="Date", inplace=True, ascending=False)
 
 
-def histogram(data):
+
+def histogram(df):
+    df['Date'] = pandas.to_datetime(df["Date"])
+    df.sort_values(by="Date", inplace=True, ascending=False)
     df["Returns"] = (df["Close"] - df["Open"]) / df["Open"]
     stats = df["Returns"].describe()
     count = df["Returns"].count()
@@ -43,52 +37,62 @@ def histogram(data):
         2:(std_count[2]/count),
         3:(std_count[3]/count)
     }
-
-    statistics = {1:std_percent, 2:df["Returns"]}
+    returns = df['Returns'].to_dict()
+    statistics = {"Percents":std_percent,"Returns":returns}
    
     return statistics
 
-# histo = histogram(df)
 
 
 
 
-def atr(data,timeframe):
-    data["High to Low"] = (data["High"] - data["Low"])
-    data["ATR"] = ((data["High to Low"] + data["High to Low"].shift(-1))/2)/data["Open"].shift(-1)
-    data["ATR"].drop(data["ATR"].index[-1], inplace=True)
+
+def atr(df,timeframe):
+    
+    df['Date'] = pandas.to_datetime(df["Date"])
+    df.sort_values(by="Date", inplace=True, ascending=False)
+
+    df["High to Low"] = (df["High"] - df["Low"])
+    df["ATR"] = ((df["High to Low"] + df["High to Low"].shift(-1))/2)/df["Open"].shift(-1)
+    df["ATR"].drop(df["ATR"].index[-1], inplace=True)
 
     average_atr = {}
     if timeframe == "Daily":
         average_atr = {
-            "1Week":data["ATR"].head(5).describe()["mean"],
-            "1Month":data["ATR"].head(25).describe()["mean"],
-            "3Months":data["ATR"].head(75).describe()["mean"],
-            "1Year":data["ATR"].head(251).describe()["mean"],
-            "2Years":data["ATR"].head(501).describe()["mean"]
+            "1Week":df["ATR"].head(5).describe()["mean"],
+            "1Month":df["ATR"].head(25).describe()["mean"],
+            "3Months":df["ATR"].head(75).describe()["mean"],
+            "1Year":df["ATR"].head(251).describe()["mean"],
+            "2Years":df["ATR"].head(501).describe()["mean"]
         }
         
     
     elif timeframe == "Weekly":
         average_atr = {
-            "12Weeks":data["ATR"].head(12).describe()["mean"],
-            "26Weeks":data["ATR"].head(26).describe()["mean"],
-            "52Weeks":data["ATR"].head(52).describe()["mean"],
-            "104Weeks":data["ATR"].head(104).describe()["mean"],
-            "156Weeks":data["ATR"].head(156).describe()["mean"]
+            "12Weeks":df["ATR"].head(12).describe()["mean"],
+            "26Weeks":df["ATR"].head(26).describe()["mean"],
+            "52Weeks":df["ATR"].head(52).describe()["mean"],
+            "104Weeks":df["ATR"].head(104).describe()["mean"],
+            "156Weeks":df["ATR"].head(156).describe()["mean"]
         }
        
     
     elif timeframe == "Monthly":
         average_atr = {
-            "6Months":data["ATR"].head(6).describe()["mean"],
-            "12Months":data["ATR"].head(12).describe()["mean"],
-            "24Months":data["ATR"].head(24).describe()["mean"],
-            "36Months":data["ATR"].head(36).describe()["mean"],
-            "60Months":data["ATR"].head(60).describe()["mean"]
+            "6Months":df["ATR"].head(6).describe()["mean"],
+            "12Months":df["ATR"].head(12).describe()["mean"],
+            "24Months":df["ATR"].head(24).describe()["mean"],
+            "36Months":df["ATR"].head(36).describe()["mean"],
+            "60Months":df["ATR"].head(60).describe()["mean"]
         }
+    else:
+        return "Wrong Timeframe"
         
     return average_atr
     
     
-print(atr(dfmon,"Monthly"))
+
+# def toJson():
+#     for row in data.iterrows():
+#         "data": row["returns"]
+#     pass
